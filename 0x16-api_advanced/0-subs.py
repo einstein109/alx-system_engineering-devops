8e196a35e92a99116f3911909ceb9903f031336a
Einstein Cheruiyot <cheruiyoteinstein@gmail.com>
2023-08-09 12:54:46 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,15 +3,21 @@
 Contains the number_of_subscribers function
 """
 
-import requests
-
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+  # Set a custom User-Agent header to avoid errors
+  headers = {"User-Agent": "Bing Chat Bot"}
+  # Construct the URL for the subreddit
+  url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+  # Send a GET request to the URL and get the response
+  response = requests.get(url, headers=headers)
+  # Check if the response status code is 200 (OK)
+  if response.status_code == 200:
+    # Parse the JSON data from the response
+    data = response.json()
+    # Get the number of subscribers from the data
+    subscribers = data["data"]["subscribers"]
+    # Return the number of subscribers
+    return subscribers
+  else:
+    # If the response status code is not 200, return 0
+    return 0
